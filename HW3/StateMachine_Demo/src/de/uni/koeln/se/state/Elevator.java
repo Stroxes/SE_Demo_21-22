4ccee5125d81497e81e1ec122aed56faeaa99534
cthaqi <c.thaqigmail.com>
2021-10-30 17:39:03 +0200

    methode arrive_atFloor()

--- HW3/StateMachine_Demo/src/de/uni/koeln/se/state/Elevator.java
+++ HW3/StateMachine_Demo/src/de/uni/koeln/se/state/Elevator.java
@@ -13,12 +13,23 @@ public class Elevator{
 		this.dest_Floor=dest_floor;
 	System.out.println("*** Destination floor is: "+ dest_Floor);
 	arrive_atFloor();
-
+	
 
 	}
 
 	private void arrive_atFloor() {
-		
+		while(dest_Floor!=current_Floor) {
+			if(dest_Floor>current_Floor) {
+				State.Current_State=State.Moving_up;
+				current_Floor++;
+			}
+			if(dest_Floor<current_Floor) {
+				State.Current_State=State.Moving_down;
+				current_Floor--;
+			}
+			
+		}
+		System.out.println("You arrived at the destination Floor:");
 	}
 
 	
--- HW3/StateMachine_Demo/src/de/uni/koeln/se/state/Elv_States.java
+++ HW3/StateMachine_Demo/src/de/uni/koeln/se/state/Elv_States.java
@@ -2,7 +2,12 @@ package de.uni.koeln.se.state;
 
 public class Elv_States {
 	
-	public  String Moving_up = "Moving_up";
-
+	public String Moving_up = "Moving_up";
+	public String Moving_down = "Moving_down";
+	public String Current_State;
+	public Elv_States() {
+		
+	}
+	
 
 }
